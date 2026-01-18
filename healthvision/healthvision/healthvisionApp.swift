cdff2380e3dc54b4282bd07e6e93893f11a464fd
Tony Chen <tonychengangyi@gmail.com>
2026-01-17 16:56:16 -0800

    Working pursage app.

---
+++ healthvision/healthvision/healthvisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  healthvisionApp.swift
+//  healthvision
+//
+//  Created by Tony Chen on 2026-01-17.
+//
+
+import SwiftUI
+
+@main
+struct healthvisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ healthvision/healthvisionTests/healthvisionTests.swift
@@ -0,0 +1,17 @@
+//
+//  healthvisionTests.swift
+//  healthvisionTests
+//
+//  Created by Tony Chen on 2026-01-17.
+//
+
+import Testing
+@testable import healthvision
+
+struct healthvisionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
