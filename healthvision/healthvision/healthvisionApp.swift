0abfb92a7cfb6627c923535f8150d6061d51d968
Tony Chen <tonychengangyi@gmail.com>
2026-01-17 16:57:04 -0800

    Editted and worked swift.

--- healthvision/healthvision/healthvisionApp.swift
+++ healthvision/healthvision/healthvisionApp.swift
@@ -6,6 +6,20 @@
 //
 
 import SwiftUI
+import SmartSpectraSwiftSDK
+
+struct ContentView: View {
+    @ObservedObject var sdk = SmartSpectraSwiftSDK.shared
+    
+    init() {
+        let apiKey = "oQkCjMU4z0asFsSaQeluhG9Ng94jtFi4u5ucy3E4"
+        sdk.setApiKey(apiKey)
+    }
+    
+    var body: some View {
+        SmartSpectraView()
+    }
+}
 
 @main
 struct healthvisionApp: App {
